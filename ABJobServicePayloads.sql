fb5e19c8e030c9cf32dfe11e55477e7d397436c6
GavinHolyday <74916323+GavinHolyday@users.noreply.github.com>
2023-03-03 11:50:59 +0000

    Update ABJobServicePayloads.sql

--- ABJobServicePayloads.sql
+++ ABJobServicePayloads.sql
@@ -1,4 +1,4 @@
 SELECT J.ID, J.Created, CAST(JP.Payload AS VARCHAR(MAX)) AS Payload
-FROM Jobs.JobPayload JP
-JOIN Jobs.Job J ON J.ID = JP.JobID
+FROM dbo.JobPayload JP
+JOIN dbo.Job J ON J.ID = JP.JobID
 WHERE J.Created BETWEEN '2023-03-02 00:00:00' AND '2023-03-02 23:59:59'
